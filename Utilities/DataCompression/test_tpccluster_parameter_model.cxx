31f2bc36c6a5ac02af2da53f36f26b9853d920bc
Matthias Richter <Matthias.Richter@scieq.net>
2016-09-28 15:19:49 +0200

    Adding compile time switches and unrolled bulk operation
    Adding unrolling of the encode loop, activated by compiler flag
    -DUNROLL
    Adding compile time switch for conditional build of decoding check,
    compiler floag -DCHECK_DECODE

--- Utilities/DataCompression/test_tpccluster_parameter_model.cxx
+++ Utilities/DataCompression/test_tpccluster_parameter_model.cxx
@@ -12,28 +12,36 @@ g++ --std=c++11 -g -ggdb -I$BOOST_ROOT/include -o test_tpccluster_parameter_mode
 #include <chrono>
 #include <boost/mpl/size.hpp>
 #include <boost/mpl/string.hpp>
+#include <boost/mpl/at.hpp>
 
 typedef std::chrono::system_clock system_clock;
 typedef std::chrono::microseconds TimeScale;
 
-template<typename DispatcherT>
+template<typename DispatcherT, bool _doEncoding = true, bool _checkDecoding = false>
 struct encodeValue {
   encodeValue(DispatcherT& _d, int _verbosity) : dispatcher(_d), verbosity(_verbosity) {}
   ~encodeValue() {}
-  int operator()(unsigned long long int value) {
+
+  typedef DispatcherT dispatcher_type;
+  static const bool doEncoding = _doEncoding;
+  static const bool checkDecoding = _checkDecoding;
+
+  int operator()(unsigned long long int value, typename DispatcherT::code_type& code = 0, uint16_t& codelen = 0) {
   // TODO: the dispatcher forwards the code type of the upper most
   // container stage
-  typename DispatcherT::code_type code;
-  uint16_t codelen = 0;
 
-  // encode the value
-  dispatcher.encode(value, code, codelen, false);
+    if (doEncoding) {
+  // encode the value and increment model if no subsequent decoding
+  // this is a compile time switch
+  dispatcher.encode(value, code, codelen, !checkDecoding);
   if (verbosity > 1) {
     std::cout << "value=" << value
               << " codelen=" << codelen
               << " code=" << code
               << std::endl;
   }
+    }
+  if (!checkDecoding) return 0;
 
   // move to upper most bits if order MSB
   // this function gets the coding direction for model at current stage
@@ -58,7 +66,12 @@ template<typename DispatcherT>
 struct incrementModel {
   incrementModel(DispatcherT& _d, int _verbosity) : dispatcher(_d), verbosity(_verbosity) {}
   ~incrementModel() {}
-  int operator()(unsigned long long int value) {
+
+  typedef DispatcherT dispatcher_type;
+  static const bool doEncoding = true;
+  static const bool checkDecoding = false;
+
+  int operator()(unsigned long long int value, typename DispatcherT::code_type& code = 0, uint16_t& codelen = 0) {
     if (verbosity > 1) {
       std::cout << "increment occurrence for value " << value << std::endl;
     }
@@ -72,11 +85,18 @@ struct incrementModel {
 template<typename F>
 int processClusters(const char* filename, F action)
 {
+  bool unroll = !F::doEncoding;
+
   int nofClusters = 0;
   if (action.verbosity > 0) {
     std::cout << "Processing file " << filename << std::endl;
   }
   ALICE::HLT::TPC::RawClusterArray ca(filename);
+
+  typename F::dispatcher_type::container_type& container = *action.dispatcher;
+  typedef typename F::dispatcher_type::container_type::types wrapped_types;
+  typename F::dispatcher_type::code_type code;
+  uint16_t codelen = 0;
 
   uint16_t lastPadrow = 0;
   for (auto cluster : ca) {
@@ -89,25 +109,46 @@ int processClusters(const char* filename, F action)
     uint16_t padrow = cluster.GetPadRow();
     unsigned long long int value = padrow - lastPadrow;
     lastPadrow = padrow;
-    action(value);
+    if (unroll) {
+      code = (*static_cast<typename boost::mpl::at_c<wrapped_types, 0>::type&>(container)).Encode(value, codelen);
+    }
+    action(value, code, codelen);
     float padval = cluster.GetPad() * 60;
     value = padval;
-    action(value);
+    if (unroll) {
+      code =  (*static_cast<typename boost::mpl::at_c<wrapped_types, 1>::type&>(container)).Encode(value, codelen);
+    }
+    action(value, code, codelen);
     float timeval = cluster.GetTime() * 25;
     value = timeval;
-    action(value);
+    if (unroll) {
+      code =  (*static_cast<typename boost::mpl::at_c<wrapped_types, 2>::type&>(container)).Encode(value, codelen);
+    }
+    action(value, code, codelen);
     float sigmaPad2val = cluster.GetSigmaPad2() * 25;
     value = sigmaPad2val;
     if (value > 255) value = 255;
-    action(value);
+    if (unroll) {
+      code =  (*static_cast<typename boost::mpl::at_c<wrapped_types, 3>::type&>(container)).Encode(value, codelen);
+    }
+    action(value, code, codelen);
     float sigmaTime2val = cluster.GetSigmaTime2() * 10;
     value = sigmaTime2val;
     if (value > 255) value = 255;
-    action(value);
+    if (unroll) {
+      code =  (*static_cast<typename boost::mpl::at_c<wrapped_types, 4>::type&>(container)).Encode(value, codelen);
+    }
+    action(value, code, codelen);
     value = cluster.GetCharge();
-    action(value);
+    if (unroll) {
+      code =  (*static_cast<typename boost::mpl::at_c<wrapped_types, 5>::type&>(container)).Encode(value, codelen);
+    }
+    action(value, code, codelen);
     value = cluster.GetQMax();
-    action(value);
+    if (unroll) {
+      code =  (*static_cast<typename boost::mpl::at_c<wrapped_types, 6>::type&>(container)).Encode(value, codelen);
+    }
+    action(value, code, codelen);
   }
   return nofClusters;
 }
@@ -117,6 +158,20 @@ int main()
   int verbosity = 0;
   enum {unknown = 0, training = 1, encoding = 2};
   int mode = encoding;
+#ifdef VERBOSE
+  verbosity = VERBOSE;
+#endif
+#ifdef UNROLL
+  const bool doUnroll = true;
+#else
+  const bool doUnroll = false;
+#endif
+#ifdef CHECK_DECODING
+  const bool doDecoding = true;
+#else
+  const bool doDecoding = false;
+#endif
+
   std::cout << "Testing multi parameter definition:" << std::endl;
 
   typedef ALICE::O2::CodingModelDispatcher< tpccluster_parameter_models > TPCClusterParameterDispatcher_t;
@@ -150,7 +205,7 @@ int main()
       system_clock::time_point refTimeAction = system_clock::now();
       switch (mode) {
       case encoding:
-	totalNofClusters += processClusters(line.c_str(), encodeValue<TPCClusterParameterDispatcher_t>(multiparameterdispatcher, verbosity));
+	totalNofClusters += processClusters(line.c_str(), encodeValue<TPCClusterParameterDispatcher_t, !doUnroll, doDecoding>(multiparameterdispatcher, verbosity));
 	break;
       case training:
 	totalNofClusters += processClusters(line.c_str(), incrementModel<TPCClusterParameterDispatcher_t>(multiparameterdispatcher, verbosity));

0018908e548c92e72f872747843316a7e514c131
Matthias Richter <Matthias.Richter@scieq.net>
2017-01-19 23:37:46 +0100

    Adjusting the test macro
    This macro was obviously out of synch, reactivated some basic tests.
    It still can not be run directly from the command line in root batch mode.

--- DataFormats/Headers/test/test.C
+++ DataFormats/Headers/test/test.C
@@ -1,19 +1,18 @@
-test() {
-gROOT->LoadMacro("DataHeader.cxx++");
-AliceO2::Base::ROOTobjectHeader m;
-AliceO2::Base::DataHeader d;
-auto b = AliceO2::Base::BaseHeader::compose(d,m);
-AliceO2::Base::BaseHeader* h = AliceO2::Base::BaseHeader::get(b.first.get());
-hexDump("",h,b.second);
-h->get(AliceO2::Base::ROOTobjectHeader::sHeaderType);
-AliceO2::Base::BaseHeader* hr = h->get(AliceO2::Base::ROOTobjectHeader::sHeaderType);
-hexDump("",hr,hr->size());
-h->get(AliceO2::Base::ROOTobjectHeader::sHeaderType);
-h->get("oiuoiu");
-h->get("ROOTmtd");
-h->get("BaseHDR");
-h->get("BaseHDe");
-string s{"asdgasdgasgd"};
-auto bs = AliceO2::Base::BaseHeader::compose(s);
-hexDump("",bs.first.get(),bs.second);
+#include "Headers/DataHeader.h"
+
+void test() {
+  gROOT->LoadMacro("DataHeader.cxx++");
+  AliceO2::Header::DataHeader d;
+  d.dataDescription = AliceO2::Header::DataDescription("TESTDATASAMPLE");
+  d.dataOrigin = AliceO2::Header::DataOrigin("TEST");
+  AliceO2::Header::NameHeader<16> n("AUXILIARYINFO");
+  auto b = AliceO2::Header::Block::compose(d,n);
+  AliceO2::Header::hexDump("Block dump",b.data(),b.size());
+  for (const AliceO2::Header::BaseHeader* h = b; h != nullptr; h = h->next()) {
+    AliceO2::Header::hexDump("Header dump",h->data(),h->size());
+  }
+
+  // find header in the data block by type
+  auto sh = AliceO2::Header::get<AliceO2::Header::NameHeader<16>>(b.data());
+  AliceO2::Header::hexDump("Header dump",sh->data(),sh->size());
 }

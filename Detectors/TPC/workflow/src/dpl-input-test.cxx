585c1431e3e2c1934d5b59518f8e42bab9ed75bb
Matthias Richter <Matthias.Richter@scieq.net>
2018-06-28 08:50:33 +0200

    writing metrics to tree

--- Detectors/TPC/workflow/src/dpl-input-test.cxx
+++ Detectors/TPC/workflow/src/dpl-input-test.cxx
@@ -8,6 +8,8 @@
 #include "DataFormatsTPC/ClusterHardware.h"
 #include "DataFormatsTPC/Helpers.h"
 #include "TPCReconstruction/HardwareClusterDecoder.h"
+#include <TFile.h>
+#include <TTree.h>
 
 // we need to add workflow options before including Framework/runDataProcessing
 void customize(std::vector<o2::framework::ConfigParamSpec>& workflowOptions)
@@ -105,21 +107,45 @@ WorkflowSpec defineDataProcessing(ConfigContext const& configcontext)
     if (++rollCount >= nRolls) {
       LOG(INFO) << "processed " << rollCount;
       ctx.services().get<ControlService>().readyToQuit(true);
-      int lastTime = -1;
-      int lastId = -1;
+      int varRolls = nRolls;
+      int varChannels = nChannels;
+      int varPages = nPages;
+      int varSize = nPages * nChannels;
+      int varId = -1;
+      int varTime = -1;
+      int varTimeDiff = 0;
+      int varFrameworkTime = 0;
       int set = 0;
+      std::string filename = "metrics_" + std::to_string(nRolls) + "_" + std::to_string(nChannels) + "_" + std::to_string(nPages) + ".root";
+      auto file = std::make_unique<TFile>(filename.c_str(), "RECREATE");
+      auto tree = std::make_unique<TTree>("metrics", filename.c_str());
+      tree->Branch("rolls", &varRolls, "rolls/I");
+      tree->Branch("channels", &varChannels, "channels/I");
+      tree->Branch("pages", &varPages, "pages/I");
+      tree->Branch("size", &varSize, "size/I");
+      tree->Branch("id", &varId, "id/I");
+      tree->Branch("time", &varTime, "time/I");
+      tree->Branch("diff", &varTimeDiff, "diff/I");
+      tree->Branch("framework", &varFrameworkTime, "framework/I");
       for (auto & metric : *metrics) {
-	if (lastId >= 0 && lastId >= metric.first) {
+	varTimeDiff = (varTime > 0 ? metric.second - varTime : 0);
+	if (varId >= 0 && varId >= metric.first) {
 	  set++;
+	  varFrameworkTime = varTimeDiff;
 	}
 	LOG(INFO) << "Metrics dump (" << nRolls << "/" << nChannels << "/" << nPages << "): "
 		  << std::setw(4) << set << " "
 		  << std::setw(2) << metric.first << " "
 		  << std::setw(8) << metric.second << " "
-		  << std::setw(8) << (lastTime > 0 ? metric.second - lastTime : 0);
-	lastTime = metric.second;
-	lastId = metric.first;
+		  << std::setw(8) << varTimeDiff << " "
+		  << std::setw(8) << varFrameworkTime;
+	varTime = metric.second;
+	varId = metric.first;
+	tree->Fill();
       }
+      tree->Write();
+      tree.release();
+      file->Close();
     }
   };
   };
